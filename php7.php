1bafbe646c3d03df0c7340ccfc6918b325a1732a
bitserikacosta <43554972+bitserikacosta@users.noreply.github.com>
2019-03-07 11:42:12 -0500

    Update php7.php

--- php7.php
+++ php7.php
@@ -1,29 +1,29 @@
 <?php
-	function calculadora($num1, $num2, $operacion)
+function calculadora($num1, $num2, $operacion)
 {
-	switch ($operacion) {
-		case '+':
-			$resultado = $num1 + $num2;
-			echo $num1 . ' + ' . $num2 . " = $resultado<br>";
-			break;
+    switch ($operacion) {
+        case '+':
+            $resultado = $num1 + $num2;
+            echo $num1 . ' + ' . $num2 . " = $resultado<br>";
+            break;
 
-		case '-':
-			$resultado = $num1 - $num2;
-			echo $num1 . ' - ' . $num2 . " = $resultado<br>";
-			break;
+        case '-':
+            $resultado = $num1 - $num2;
+            echo $num1 . ' - ' . $num2 . " = $resultado<br>";
+            break;
 
-		case '*':
-			$resultado = $num1 * $num2;
-			echo $num1 . ' * ' . $num2 . " = $resultado<br>";
-			break;
+        case '*':
+            $resultado = $num1 * $num2;
+            echo $num1 . ' * ' . $num2 . " = $resultado<br>";
+            break;
 
-		case '/':
-			if ($num2 == 0) {
-				echo $num1 . ' / ' . $num2 . " = ERROR0<br><br>";
-			} else {
-				$resultado = $num1 / $num2;
-				echo $num1 . " / " . $num2 . " = $resultado<br><br>";
-			}
-	}
+        case '/':
+            if ($num2 == 0) {
+                echo $num1 . ' / ' . $num2 . " = ERROR0<br><br>";
+            } else {
+                $resultado = $num1 / $num2;
+                echo $num1 . " / " . $num2 . " = $resultado<br><br>";
+            }
+    }
 }
 ?>
