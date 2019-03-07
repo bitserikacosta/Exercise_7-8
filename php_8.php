c4e658fd5e3f17eb690ddde63076e02113e0d958
bitserikacosta <43554972+bitserikacosta@users.noreply.github.com>
2019-03-07 11:43:07 -0500

    Update php_8.php

--- php_8.php
+++ php_8.php
@@ -1,19 +1,18 @@
 <?php
-	include "php7.php";
+include "php7.php";
 
 $numeros = array(
-	array('num1' => 41, 'num2' => 2.1),
-	array('num1' => 3, 'num2' => 67),
-	array('num1' => 145, 'num2' => 0),
-	array('num1' => 316, 'num2' => 5150)
+    array('num1' => 41, 'num2' => 2.1),
+    array('num1' => 3, 'num2' => 67),
+    array('num1' => 145, 'num2' => 0),
+    array('num1' => 316, 'num2' => 5150)
 );
 
 $operaciones = array('Suma' => '+', 'Resta' => '-', 'Multiplicacion' => '*', 'Division' => '/');
 
 foreach ($numeros as $factores) {
-	foreach ($operaciones as $simbolo) {
-
-		calculadora($factores["num1"], $factores["num2"], $simbolo);
-	}
+    foreach ($operaciones as $simbolo) {
+        calculadora($factores["num1"], $factores["num2"], $simbolo);
+    }
 }
 ?>
