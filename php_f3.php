ea29a617ba88b73eb0c011b2d43a419ff77617c8
bitserikacosta <43554972+bitserikacosta@users.noreply.github.com>
2019-03-18 08:16:18 -0500

    Add files via upload

---
+++ php_f3.php
@@ -0,0 +1,21 @@
+<?php
+$countries = [ 'uk' => 'United Kingdom', 'fr' => 'France', 'pt' => 'Portugal', 've' => 'Venezuela', 'at' => 'Austria', 'es' => 'Spain', 'co' => 'Colombia' ];
+
+$myCountry = "Colombia";
+if (in_array("$myCountry", $countries)) 
+{ 
+echo "Mi país de origen es " . $myCountry . " y está en la lista <hr>"; 
+} 
+else
+{ 
+echo "Mi país de origen es " . $myCountry . " pero no está en la lista <hr>"; 
+} 
+
+asort($countries);
+
+foreach($countries as $x => $x_value) {
+    echo $x . " -> " . $x_value;
+    echo "<br>";
+}
+echo "<hr>";
+print_r($countries);
