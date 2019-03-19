bb59930aed0659932401dcf108358d4a6c2b2be0
bitserikacosta <43554972+bitserikacosta@users.noreply.github.com>
2019-03-19 11:02:44 -0500

    Update php_f4.php

--- php_f4.php
+++ php_f4.php
@@ -1,24 +1,30 @@
 <?php
-
-
-   $dates =  array(
+$dates = array(
     array('day' => 3, 'month' => 12, 'year' => 2005),
     array('day' => 30, 'month' => 2, 'year' => 2017),
-	      array('day' => 7, 'month' => 9, 'year' => 2019),
-    array('day' => 1, 'month' => 11, 'year' => 2014),
-   );
-$dd=$dates[0]; // first element of the array is date
-$mm=$dates[1]; // second element is month
-$yy=$dates[2]; // third element is year
+    array('day' => 1, 'month' => 11, 'year' => 2014)
 
-for($i=0;$i<count($dates);$i++){
- $c=0;
- foreach($dates[$i] as $key=>$value){
-  $c++;
-  echo $key."=".$value;
-  if($c<count($dates[$i])) echo ",";
- }
- echo "<hr>";
-echo var_dump(checkdate($dd,$mm,$yy));
+);
+//Añade una nueva entrada al array con la fecha actual, en (day, month, year) como en timestamp. La fecha actual se tiene que obtener dinámicamente.
+$dates[] = array('day'=>date("d"), 'month'=>date("m"), 'year'=>date("Y"));
 
+$numbers = date("d/m/Y");
+$array =  explode(",", $numbers);
+$toUnset= 0;
+//Recorre el array y comprueba si cada fecha es válida. Si encuentras una fecha no válida, elimínala del array (unset())
+foreach ($dates as $key) {
+    if (count($key) == 3) {
+        if (checkdate($key['month'], $key['day'], $key['year']) == true) {
+            $DateValues = strtotime(implode(".", $key));
+            //Para cada elemento del array, añade un nuevo índice 'timestamp', y asígnale el valor de cada fecha en formato timestamp.
+            $dates[$toUnset]['Timestamp: '] = $DateValues;
+        } else {
+            unset($dates[$toUnset]);
+        }
+    }
+    $toUnset++;
 }
+//Imprime el array resultante con print_r().
+print_r($dates);
+
+?>
